a90955747863796157edcbb2f3a4b5a8c4de0523
Brandon High <highb@users.noreply.github.com>
2022-06-14 21:35:42 -0700

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -10,6 +10,7 @@
     +'quit'
 
 cp ${BAR_CONFIG_IMPORT_DIR}/* ${STEAMAPPDIR}/ 
+chown -R steam:steam "${BAR_CONFIG_IMPORT_DIR}"
 
 # Update settings.xml using ENV varaibles
 SETTINGS_XML=${STEAMAPPDIR}/serversettings.xml
